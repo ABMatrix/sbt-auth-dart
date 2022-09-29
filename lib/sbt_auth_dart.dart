8bf66a13e4fb30f93d52e62bd7f6637a7a09c0e7
xijieyin <xijieyin@gmail.com>
2022-09-29 19:32:35 +0800

    chore: initial commit

---
+++ lib/sbt_auth_dart.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// SBTAuth flutter sdk
+library sbt_auth_dart;
+
+export 'src/sbt_auth_dart.dart';
